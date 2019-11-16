1ccaa095e864c953e7b08e2eefac6a83aa7e9aea
gabrielibagon <gabriel.ibagon@gmail.com>
2019-11-16 13:44:29 -0800

    fixed gaussian blur example

--- examples/simple_gaussian_blur.py
+++ examples/simple_gaussian_blur.py
@@ -1,12 +1,12 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-
 from tensorblur.gaussian import GaussianBlur
 
 # Load an image
 init_img = np.array(Image.open("assets/example2.jpg"))
 
+# Iterate over different blur sizes
 for size in [2, 4, 8, 16, 32, 64]:
 
     # Make a fresh copy of the image
@@ -16,27 +16,12 @@ for size in [2, 4, 8, 16, 32, 64]:
     gauss = GaussianBlur(size=size)
 
     # Apply blurring
-    img = gauss.apply(img)
+    result = gauss.apply(img)
+    result = result.numpy().astype(int)
 
     # Display result
     plt.title('Gaussian Blur | Size: %i' % size)
-    plt.imshow(img[0, ...].numpy().astype(int))
+    plt.imshow(result)
+    plt.axis('off')
     plt.show()
 
-
-    # Apply blurring
-    img = gauss.apply(img)
-
-    # Display result
-    plt.title('Gaussian Blur | Size: %i' % size)
-    plt.imshow(img[0, ...].numpy().astype(int))
-    plt.show()
-
-
-    # Apply blurring
-    img = gauss.apply(img)
-
-    # Display result
-    plt.title('Gaussian Blur | Size: %i' % size)
-    plt.imshow(img[0, ...].numpy().astype(int))
-    plt.show()
